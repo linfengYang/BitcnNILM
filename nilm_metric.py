7d6e42c6108b5e5c9361cfe1c6e2fa0dd10942bd
linfengYang <eidp_gxu@126.com>
2020-11-25 10:15:18 +0800

    Add files via upload

--- nilm_metric.py
+++ nilm_metric.py
@@ -1,5 +1,6 @@
 from Logger import log
 import numpy as np
+import pandas as pd
 
 
 # from sklearn.metrics import confusion_matrix
@@ -269,3 +270,90 @@ def get_statistics(data):
     quartile2 = np.percentile(data, 75)
 
     return mean, std, min_v, max_v, quartile1, median, quartile2
+
+
+#####################################################################
+def tp_tn_fp_fn(states_pred, states_ground):
+    tp = np.sum(np.logical_and(states_pred == 1, states_ground == 1))
+    fp = np.sum(np.logical_and(states_pred == 1, states_ground == 0))
+    fn = np.sum(np.logical_and(states_pred == 0, states_ground == 1))
+    tn = np.sum(np.logical_and(states_pred == 0, states_ground == 0))
+    return tp, tn, fp, fn
+
+def recall_precision_accuracy_f1(pred, ground,threshold):
+    # aligned_meters = align_two_meters(pred, ground)
+    # data = {
+    #     'pred': pred,
+    #     'truth': ground
+    # }
+    # df = pd.DataFrame(data)
+    threshold = threshold
+    chunk_results = []
+
+    sum_samples = len(pred)
+    pr = np.array([0 if (p)<threshold else 1 for p in pred])
+    gr = np.array([0 if p<threshold else 1 for p in ground])
+
+    tp, tn, fp, fn = tp_tn_fp_fn(pr,gr)
+    p = sum(pr)
+    n = len(pr) - p
+
+    chunk_results.append([tp,tn,fp,fn,p,n])
+
+    if sum_samples == 0:
+        return None
+    else:
+        # [tp,tn,fp,fn,p,n] = np.sum(chunk_results, axis=0)
+
+        res_recall = recall(tp,fn)
+        res_precision = precision(tp,fp)
+        res_f1 = f1(res_precision,res_recall)
+        res_accuracy = accuracy(tp,tn,p,n)
+
+        return (res_recall,res_precision,res_accuracy,res_f1)
+
+def relative_error_total_energy(pred, ground):
+    aligned_meters = align_two_meters(pred, ground)
+    chunk_results = []
+    sum_samples = 0.0
+    for chunk in aligned_meters:
+        chunk.fillna(0, inplace=True)
+        sum_samples += len(chunk)
+        E_pred = sum(chunk.iloc[:,0])
+        E_ground = sum(chunk.iloc[:,1])
+
+        chunk_results.append([
+                            E_pred,
+                            E_ground
+                            ])
+    if sum_samples == 0:
+        return None
+    else:
+        [E_pred, E_ground] = np.sum(chunk_results,axis=0)
+        return abs(E_pred - E_ground) / float(max(E_pred,E_ground))
+
+def mean_absolute_error(pred, ground):
+    aligned_meters = align_two_meters(pred, ground)
+    total_sum = 0.0
+    sum_samples = 0.0
+    for chunk in aligned_meters:
+        chunk.fillna(0, inplace=True)
+        sum_samples += len(chunk)
+        total_sum += sum(abs((chunk.iloc[:,0]) - chunk.iloc[:,1]))
+    if sum_samples == 0:
+        return None
+    else:
+        return total_sum / sum_samples
+
+
+def recall(tp,fn):
+    return tp/float(tp+fn)
+
+def precision(tp,fp):
+    return tp/float(tp+fp)
+
+def f1(prec,rec):
+    return 2 * (prec*rec) / float(prec+rec)
+
+def accuracy(tp, tn, p, n):
+    return (tp + tn) / float(p + n)
